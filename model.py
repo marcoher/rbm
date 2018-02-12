73e087b7ca7a9f50237dc9311bac1c8a4515e7b6
Marco Hernandez <mens.oculus@gmail.com>
2018-02-11 21:12:49 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -2,39 +2,39 @@ from __future__ import division, print_function
 import tensorflow as tf
 import numpy as np
 
-implemented = ["binary"]
+implemented = ["binary"] # TODO: implement gaussian, binomial, etc
 
 class rbm(object):
   """
   Restricted Boltzmann Machine
   """
-  def __init__(self, visible_dim, hidden_dim, visible_type = "binary", seed = 0, name = "rbm"):
+  def __init__(self, visible_dim, hidden_dim, visible_type = "binary", seed = 1234, name = "rbm"):
     """
     visible_dim   : number of visible units
     hidden_dim    : number of hidden units
-    visible_type  : optional, type of visible units, default is "binary"
-    seed          : optional, seed for reproducibility, default is None
-    name          : optional, name of the model, default is "rbm"
+    visible_type  : optional, type of visible units, default is binary
+    seed          : optional, seed for reproducibility, default is 1234
+    name          : optional, name of the model, default is rbm
     """
 
-    assert visible_dim>0,"number of visible units must be positive: %s" % visible_dim
-    assert hidden_dim>0,"number of hidden units must be positive: %s" % hidden_dim
-    assert visible_type.strip().lower() in implemented,"type of visible units is not implemented: %s" % visible_type
+    assert (isinstance(visible_dim,int) and visible_dim>0),"visible_dim must be a positive integer: %s" % visible_dim
+    assert (isinstance(hidden_dim, int) and hidden_dim>0 ),"hidden_dim must be a positive integer: %s" % hidden_dim
+    assert visible_type.strip().lower() in implemented,"type of visible units is not implemented: %s (expected in %s)" % (visible_type, implemented)
     assert isinstance(seed, int),"seed must be an integer: %s" % seed
     assert isinstance(name, str),"name must be a string: %s" % name
 
-    self.visible_dim = visible_dim
-    self.hidden_dim  = hidden_dim
-    self.seed = seed
+    self.visible_dim  = visible_dim
+    self.hidden_dim   = hidden_dim
+    self.seed         = seed
     self.visible_type = visible_type.strip().lower()
-    self.name = name
-    self.cd_steps = None
-    self.graph = self._build()
+    self.name         = name
+    self.cd_steps     = None
+    self.graph        = None
 
-  def fit(self, data, max_epochs = 1000, learning_rate = 0.1, cd_steps = 1, verbosity = 10):
+  def fit(self, data, max_epochs = 1000, learning_rate = 0.1, cd_steps = 1, verbosity = 100):
     """
     Fit the model to a dataset
-    data          : an array of shape [num_samples, visible_dim]
+    data          : a numpy array of shape [num_samples, visible_dim]
     max_epochs    : optional, maximum number of epochs to use in training, default is 1000
     learning_rate : optional, learning rate to use in training, default is 0.1
     cd_steps      : optional, number of contrastive divergence steps, default is 1
@@ -48,7 +48,10 @@ class rbm(object):
     assert (isinstance(cd_steps, int) and cd_steps>0),"cd_steps must a positive ingeger: %s" % cd_steps
     assert (isinstance(verbosity, int) and verbosity>0),"verbosity must be a positive integer: %s" % verbosity
 
+    if not self.graph:  self._build()
+
     self._training_ops(cd_steps)
+
     train_dict = {self.visible : data, self.lr : learning_rate, self.N : data.shape[0]}
 
     with tf.Session(graph=self.graph) as sess:
@@ -72,7 +75,10 @@ class rbm(object):
     assert (isinstance(num_samples, int) and num_samples>0),"num_samples must be a positive integer: %s" % num_samples
     assert (isinstance(iters, int) and iters>0),"iters must be a positive integer: %s" % iters
 
+    if not self.graph:  self._build()
+
     hidden_seed = np.random.random_integers(0, 1, (num_samples, self.hidden_dim))
+
     with tf.Session(graph=self.graph) as sess:
       sess.run(self.init)
       for _ in xrange(iters):
@@ -85,9 +91,8 @@ class rbm(object):
     """
     Build the main graph
     """
-
-    g = tf.Graph()
-    with g.as_default():
+    self.graph = tf.Graph()
+    with self.graph.as_default():
       tf.set_random_seed(self.seed)
       self._units()
       self._variables()
@@ -96,19 +101,13 @@ class rbm(object):
       self.hidden_sample,  self.hidden_probs  = self._conditional_h_sample(self.visible)
       self.init = tf.global_variables_initializer()
 
-    return g
-
   def _units(self):
     """
     Set the data type of the visible units
     """
-    if self.visible_type in ["binary", "multinomial"]:
-
+    # TODO: this part has to be changed to other types of visible units
+    if self.visible_type in ["binary"]:
       self.visible_dtype = tf.uint8
-
-    elif self.visible_type in ["gaussian"]:
-
-      self.visible_dtype = tf.float32
 
   def _variables(self):
     """
@@ -119,21 +118,19 @@ class rbm(object):
                   name = "weights",
                   shape = [self.visible_dim, self.hidden_dim],
                   dtype = tf.float32,
-                  initializer = tf.random_uniform_initializer(
-                      minval = -0.1 * tf.sqrt(6. / (self.hidden_dim + self.visible_dim)),
-                      maxval =  0.1 * tf.sqrt(6. / (self.hidden_dim + self.visible_dim))))
+                  initializer = tf.truncated_normal_initializer(stddev=0.01))
 
       self.v_bias = tf.get_variable(
                   name = "visible_bias",
                   shape = [self.visible_dim],
                   dtype = tf.float32,
-                  initializer = tf.random_uniform_initializer())
+                  initializer = tf.zeros_initializer())
 
       self.h_bias = tf.get_variable(
                   name = "hidden_bias",
                   shape = [self.hidden_dim],
                   dtype = tf.float32,
-                  initializer = tf.random_uniform_initializer())
+                  initializer = tf.zeros_initializer())
 
   def _random_units(self):
     """
@@ -155,8 +152,9 @@ class rbm(object):
   def _conditional_v_sample(self, h):
     """
     Add ops to the main graph to sample values of the visible units given h
-    h : a tensor of shape [:, hidden_dim]
+    h : a tensor of shape [:, hidden_dim], must already be in the graph
     """
+    # TODO : change this for other types of visible units
     with tf.name_scope("visible_given_hidden"):
       v_means  = tf.sigmoid(
                     tf.matmul(tf.cast(h, tf.float32), tf.transpose(self.weights)) + self.v_bias,
@@ -171,8 +169,8 @@ class rbm(object):
 
   def _conditional_h_sample(self, v):
     """
-    Add ops to the main graph to sample values for the hidden units given v
-    v : a tensor of shape [:, visible_dim]
+    Add ops to the main graph to sample values of the hidden units given v
+    v : a tensor of shape [:, visible_dim], must already be in the graph
     """
     with tf.name_scope("hidden_given_visible"):
       h_probs  = tf.sigmoid(
@@ -191,6 +189,7 @@ class rbm(object):
     Add ops to the main graph to perform k-step Contrasting Divergence updates
     k : number of steps in contrastive divergence (cannot be modified)
     """
+    #TODO : find a way to use tf graphs so that cd_steps/k can be modified
     if self.cd_steps:   return None
 
     with self.graph.as_default():
@@ -218,11 +217,7 @@ class rbm(object):
       grad_bh = tf.reduce_mean(tf.cast(self.hidden_probs,  tf.float32) - h1_probs, axis=0)
 
       w_update  = self.weights.assign_add(self.lr * grad_w)
-      bv_update = self.v_bias.assign_add(self.lr * grad_bv)
-      bw_update = self.h_bias.assign_add(self.lr * grad_bh)
+      bv_update = self.v_bias.assign_add( self.lr * grad_bv)
+      bw_update = self.h_bias.assign_add( self.lr * grad_bh)
 
       self.update = [w_update, bv_update, bw_update]
-
-
-  #def summarize(self, path):
-#    summary_writer = tf.train.SummaryWriter(path + '/logs', graph_def = self.sess.graph_def)
